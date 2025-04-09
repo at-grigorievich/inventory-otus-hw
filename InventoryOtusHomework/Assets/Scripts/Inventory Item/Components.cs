60c2d57c3ae9e51d90dc9dec8abd683c0b435568
at-grigorievich-RUDN <at.grigorievich@gmail.com>
2025-04-09 18:03:50 +0300

    minor

--- InventoryOtusHomework/Assets/Scripts/Inventory Item/Components.cs
+++ InventoryOtusHomework/Assets/Scripts/Inventory Item/Components.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using UnityEngine;
 
 namespace ATG.OtusHW.Inventory
 {
@@ -29,6 +30,58 @@ namespace ATG.OtusHW.Inventory
             return new HeroDamageEffectComponent()
             {
                 DamageEffect = this.DamageEffect
+            };
+        }
+    }
+    
+    [Serializable]
+    public class HeroHitPointsEffectComponent : IItemComponent
+    {
+        public int HitPointsEffect = 2;
+        
+        public IItemComponent Clone()
+        {
+            return new HeroHitPointsEffectComponent()
+            {
+                HitPointsEffect = this.HitPointsEffect
+            };
+        }
+    }
+    
+    [Serializable]
+    public class HeroSpeedEffectComponent : IItemComponent
+    {
+        public int SpeedEffect = 2;
+        
+        public IItemComponent Clone()
+        {
+            return new HeroHitPointsEffectComponent()
+            {
+                HitPointsEffect = this.SpeedEffect
+            };
+        }
+    }
+
+    [Serializable]
+    public class HeroEquipmentComponent : IItemComponent
+    {
+        public EquipType Tag;
+
+        [SerializeReference] 
+        public IItemComponent[] EquipEffects;
+        
+        public IItemComponent Clone()
+        {
+            var clonedEffects = new IItemComponent[EquipEffects.Length];
+            for (int i = 0; i < EquipEffects.Length; i++)
+            {
+                clonedEffects[i] = EquipEffects[i].Clone();
+            }
+
+            return new HeroEquipmentComponent()
+            {
+                Tag = this.Tag,
+                EquipEffects = clonedEffects
             };
         }
     }
